fad006357e9d974ec8caadcdb61870d83801d035
Adam Hooper <adam@adamhooper.com>
2018-08-06 09:45:59 -0400

    shared-mime-info 1.10

--- rebuild-mime-cache.sh
+++ rebuild-mime-cache.sh
@@ -2,10 +2,10 @@
 
 set -x
 
-[ -f shared-mime-info-1.9.tar.xz ] || curl -o - --location http://freedesktop.org/~hadess/shared-mime-info-1.9.tar.xz | tar Jxf -
-(cd ./shared-mime-info && ./configure && make)
+[ -f shared-mime-info-1.10.tar.xz ] || curl -o - --location http://freedesktop.org/~hadess/shared-mime-info-1.10.tar.xz | tar Jxf -
+(cd ./shared-mime-info-1.10 && ./configure && make)
 mkdir -p d/packages
-cp shared-mime-info-1.9/freedesktop.org.xml d/packages/
+cp shared-mime-info-1.10/freedesktop.org.xml d/packages/
 cp extra-mime-info.xml d/packages/
-shared-mime-info-1.9/update-mime-database d/packages
+shared-mime-info-1.10/update-mime-database d/
 cp d/mime.cache src/main/resources/

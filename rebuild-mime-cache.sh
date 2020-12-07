740c215f3c348738fa4a46bcfcb660c55709237a
Adam Hooper <adam@adamhooper.com>
2020-12-07 15:47:04 -0500

    shared-mime-info@2.0

--- rebuild-mime-cache.sh
+++ rebuild-mime-cache.sh
@@ -2,10 +2,10 @@
 
 set -x
 
-[ -f shared-mime-info-1.13.1.tar.xz ] || curl -o - --location https://gitlab.freedesktop.org/xdg/shared-mime-info/uploads/5349e18c86eb96eee258a5c1f19122d0/shared-mime-info-1.13.1.tar.xz | tar Jxf -
-(cd ./shared-mime-info-1.13.1 && ./configure && make)
+[ -f shared-mime-info-2.0.tar.xz ] || curl -o - --location https://gitlab.freedesktop.org/xdg/shared-mime-info/-/archive/2.0/shared-mime-info-2.0.tar.gz | tar zxf -
+(cd ./shared-mime-info-2.0 && meson build && cd build && ninja src/update-mime-database data/freedesktop.org.xml)
 mkdir -p d/packages
-cp shared-mime-info-1.13.1/freedesktop.org.xml d/packages/
+cp shared-mime-info-2.0/build/data/freedesktop.org.xml d/packages/
 cp extra-mime-info.xml d/packages/
-shared-mime-info-1.13.1/update-mime-database d/
+shared-mime-info-2.0/update-mime-database -V d/
 cp d/mime.cache src/main/resources/
